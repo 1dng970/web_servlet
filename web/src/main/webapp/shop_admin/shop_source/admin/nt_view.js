2d3b15b6098b462c9353a1594c75ceb0812ae73c
1dng970 <1dng970@naver.com>
2025-03-14 17:52:56 +0900

    집가자~~~

--- web/src/main/webapp/shop_admin/shop_source/admin/nt_view.js
+++ web/src/main/webapp/shop_admin/shop_source/admin/nt_view.js
@@ -1,44 +1,23 @@
-function noticeok(){
-		
-if(frm.subject.value==""){
-	alert("공지사항 제목을 입력하세요");	
-	frm.subject.focus();
-	return false;
+function noticeok(){	
 	
-}else if(frm.writer.value==""){	
-	alert("글쓴이를 입력하세요");
-	  frm.writer.focus();	
-    return false;
-	  
-}
-
-/*
-else if(){//첨부파일 
+var nfile= document.getElementById("nfile");	
+var nfile= document.getElementById("nfile");
+	
+if(){
 	
 	
 	
-	
-}
-*/
-
-
-else{//공지내용
-
-var txt = CKEDITOR.instances.editor.getData();
-
-if(txt==""){
-	alert("내용을 입력하세요");
-		return false;
-}
-else if(txt.lengt<40){//40자이상 등록되게 조건 
-	alert("최소  40자 이상 입력하셔야 합니다.");
-		return false;
 }	
 
-else{
 
-	frm.submit();
-       }	
-   }
 	
+		
+else if(frm.subject.value==""){
+	alert("공지사항 제목을 입력하세요");	
+	
+}//첨부파일 용량
+
+else{
+	frm.submit();
+}
 }
